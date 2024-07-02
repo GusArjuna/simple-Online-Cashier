c7f6e4085c0a3476e2562e3477985a4113c73e5b
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2024-07-02 18:22:37 +0700

    feat(diagram): add seeder for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserTableSeeder::class,
+            FoodCategorySeeder::class,
+            MemberSeeder::class,
+            SupplierSeeder::class,
+            FoodSeeder::class,
+        ]);
     }
 }
