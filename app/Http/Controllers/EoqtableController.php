60a6252b4c039b361898ea38ea49a8deee3256cc
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2024-07-04 12:13:12 +0700

    feat(diagram): refactor calculation diagram

--- app/Http/Controllers/EoqtableController.php
+++ app/Http/Controllers/EoqtableController.php
@@ -21,21 +21,21 @@ class EoqtableController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function dashboard(){
+    public function dashboard()
+    {
         $foodCount = food::count();
         $memberCount = member::count();
         $supplierCount = supplier::count();
-        $foods = food::orderBy('penjualan','desc')->paginate(15);
+        $foods = food::orderBy('penjualan', 'desc')->paginate(15);
 
         $monthlyDates = $this->generateMonthlyDates('forward'); // foward or backward
-        $foodAndEoqByWeeks = $this->getEoqData($monthlyDates);
-        $groupedData = $this->groupEoqData($foodAndEoqByWeeks);
+        $groupedData = $this->getEoqData($monthlyDates);
         $products = $this->getProductNames($groupedData);
         $colors = $this->generateColors($products);
 
-        // dd($startOfWeek, $groupedData, $products, $colors);
+        // dd($monthlyDates, $groupedData, $products, $colors);
 
-        return view("dashboard",[
+        return view("dashboard", [
             "title" => "Dashboard",
             "foodCount" => $foodCount,
             "memberCount" => $memberCount,
@@ -47,17 +47,18 @@ class EoqtableController extends Controller
         ]);
     }
 
-    function generateMonthlyDates(string $direction = 'forward') {
+    function generateMonthlyDates(string $direction = 'forward')
+    {
         $dates = [];
         $currentDate = now();
-    
+
         for ($i = 0; count($dates) < 7; $i++) {
             $firstDayOfMonth = $currentDate->copy()->startOfMonth();
             $dates[] = $firstDayOfMonth->format("Y-m-d");
-    
+
             $sixteenthDayOfMonth = $currentDate->copy()->startOfMonth()->addDays(15);
             $dates[] = $sixteenthDayOfMonth->format("Y-m-d");
-    
+
             if ($direction === 'forward') {
                 $currentDate->addMonths(1);
             } elseif ($direction === 'backward') {
@@ -66,65 +67,85 @@ class EoqtableController extends Controller
                 return 'Invalid direction. Please specify "forward" or "backward".';
             }
         }
-    
+
         return $dates;
     }
 
-    function getEoqData($startOfWeek) {
+    function initFoodAndEoqWeek($startOfWeek){
         $foodAndEoqByWeeks = [];
+
+        foreach ($startOfWeek as $week) {
+            $eoqData = eoqtable::join('food', 'food.kode', 'eoqtables.kodeMakanan')
+                ->where("periode", $week)
+                ->where('EOQ', '>', 0)
+                ->orderBy('food.penjualan', 'desc')
+                ->limit(10)
+                ->get();
+
+            foreach ($eoqData as $eoq) {
+                $existingItem = array_filter($foodAndEoqByWeeks, function($item) use ($eoq){ // check exist item
+                    return $item['name'] === $eoq->nama;
+                });
+    
+                if (empty($existingItem)) {
+                    $foodAndEoqByWeeks[] = ['name' => $eoq->nama, 'data' => []];
+                }
+            }
+
+        }
+        
+        // static
+        // $foodAndEoqByWeeks[] = ['name' => "juna", 'data' => []];
+        
+        return $foodAndEoqByWeeks;
+    }
+
+    function getEoqData($startOfWeek) {
+        $foodAndEoqByWeeks = $this->initFoodAndEoqWeek($startOfWeek);
     
         foreach ($startOfWeek as $week) {
             $eoqData = eoqtable::join('food', 'food.kode', 'eoqtables.kodeMakanan')
                 ->where("periode", $week)
-                ->orderBy('EOQ', 'desc')
+                ->where('EOQ', '>', 0)
+                ->orderBy('food.penjualan', 'desc')
                 ->limit(10)
                 ->get();
     
-            foreach ($eoqData as $eoq) {
-                $foodName = $eoq->nama;
-                $eoqValue = $eoq->EOQ;
+            foreach ($foodAndEoqByWeeks as &$item) {
+                $foodName = $item['name'];
+                $eoqValue = 0;
     
-                $found = false;
-                foreach ($foodAndEoqByWeeks as &$item) {
-                    if ($item['name'] === $foodName) {
-                        $item['data'][] = $eoqValue;
-                        $found = true;
+                foreach ($eoqData as $eoq) {
+                    if ($eoq->nama === $foodName) {
+                        $eoqValue = $eoq->EOQ;
                         break;
                     }
                 }
     
-                if (!$found) {
-                    $foodAndEoqByWeeks[] = ['name' => $foodName, 'data' => [$eoqValue]];
-                }
+                $item['data'][] = $eoqValue;
             }
         }
     
         return $foodAndEoqByWeeks;
     }
-    
 
-    function groupEoqData($foodAndEoqByWeeks) {
-        $groupedData = collect($foodAndEoqByWeeks)->groupBy('name')->map(function ($item) {
-            return ['name' => $item[0]['name'], 'data' => $item->pluck('data')->flatten()->toArray()];
-        })->values()->all();
-    
-        return $groupedData;
-    }
-    
-    function getProductNames($groupedData) {
+
+    function getProductNames($groupedData)
+    {
         $products = array_map(function ($item) {
             return $item['name'];
         }, $groupedData);
-    
+
         return $products;
     }
-    
-    function generateColors($products) {
+
+    function generateColors($products)
+    {
         $colors = [];
         foreach ($products as $product) {
             $colors[] = '#' . substr(md5($product), 0, 6);
         }
-    
+
         return $colors;
     }
 
@@ -133,36 +154,36 @@ class EoqtableController extends Controller
         $foods = food::all();
         $eoqTables = eoqtable::query();
         $selectedDate = Session::get('selected_date');
-    
+
         if ($selectedDate) {
             $selectedDate = Carbon::parse($selectedDate);
             $day = $selectedDate->day;
             $periode = $selectedDate->format('Y-m') . '-' . ($day <= 15 ? '01' : '16');
             $eoqTables->where('periode', $periode);
         }
-        if(request('search')){                                     
-            $querytambahan2=food::where('kode','like','%'.request('search').'%')
-                                        ->orWhere('nama','like','%'.request('search').'%')->get()
-                                        ->orWhere('qty','like','%'.request('search').'%')->get()
-                                        ->orWhere('safetyStoc','like','%'.request('search').'%')->get()
-                                        ->orWhere('lifeTime','like','%'.request('search').'%')->get();
-                                        
-            $eoqTables->where('kodeMakanan','like','%'.request('search').'%')
-                                        ->orWhere('EOQ','like','%'.request('search').'%')
-                                        ->orWhere('ROP','like','%'.request('search').'%')
-                                        ->orWhere('biayaPenyimpanan','like','%'.request('search').'%');
-            
-            foreach($querytambahan2 as $querytambahan){
-                $querybantuan= (string)$querytambahan->kode;
-                $eoqTables->orWhere('kodeMakanan','like','%'.$querybantuan.'%');
+        if (request('search')) {
+            $querytambahan2 = food::where('kode', 'like', '%' . request('search') . '%')
+                ->orWhere('nama', 'like', '%' . request('search') . '%')->get()
+                ->orWhere('qty', 'like', '%' . request('search') . '%')->get()
+                ->orWhere('safetyStoc', 'like', '%' . request('search') . '%')->get()
+                ->orWhere('lifeTime', 'like', '%' . request('search') . '%')->get();
+
+            $eoqTables->where('kodeMakanan', 'like', '%' . request('search') . '%')
+                ->orWhere('EOQ', 'like', '%' . request('search') . '%')
+                ->orWhere('ROP', 'like', '%' . request('search') . '%')
+                ->orWhere('biayaPenyimpanan', 'like', '%' . request('search') . '%');
+
+            foreach ($querytambahan2 as $querytambahan) {
+                $querybantuan = (string)$querytambahan->kode;
+                $eoqTables->orWhere('kodeMakanan', 'like', '%' . $querybantuan . '%');
             }
         }
-        return view('EOQMethod/EOQMethod',[
-            "title"=>"EOQMethod",
+        return view('EOQMethod/EOQMethod', [
+            "title" => "EOQMethod",
             "eoqTables" => $eoqTables->paginate(15),
             "foods" => $foods,
             "selectedDate" => Session::get('selected_date'),
-            ]);
+        ]);
     }
 
     /**
@@ -222,7 +243,7 @@ class EoqtableController extends Controller
         // return redirect('/eoq')->with('success','Data Di Update');
     */
     public function updateEoq(Request $request)
-    {      
+    {
         $foods = food::all();
         $selectedDate = Carbon::parse($request->tanggal);
         $day = $selectedDate->day;
@@ -239,8 +260,8 @@ class EoqtableController extends Controller
         $totalDays = $startDate->diffInDays($endDate) + 1;
         foreach ($foods as $food) {
             $existingRecord = eoqtable::where('kodeMakanan', $food->kode)
-            ->where('periode', $check->format('Y-m') . '-' . ($day <= 15 ? '01' : '16'))
-            ->first();
+                ->where('periode', $check->format('Y-m') . '-' . ($day <= 15 ? '01' : '16'))
+                ->first();
 
             if (!$existingRecord) {
                 $hodingCost = ceil(($food->hargaJual * $food->qty) / ((100 / 100) + (20 / 100)));
@@ -260,15 +281,14 @@ class EoqtableController extends Controller
                     'BiayaPenyimpanan' => $hodingCost,
                     'EOQ' => $eoq,
                     'ROP' => $rop,
-                    'periode' => ($day <= 15 ? $startDate->copy()->addMonth()->format('Y-m') . '-' . ($day <= 15 ? '01' : '16') : $startDate->copy()->format('Y-m') . '-' . ($day <= 15 ? '01' : '16')), 
+                    'periode' => ($day <= 15 ? $startDate->copy()->addMonth()->format('Y-m') . '-' . ($day <= 15 ? '01' : '16') : $startDate->copy()->format('Y-m') . '-' . ($day <= 15 ? '01' : '16')),
                 ]);
             }
-            
         }
         Session::put('selected_date', $request->tanggal);
-        return redirect('/eoq')->with('success','Data Di Update');
+        return redirect('/eoq')->with('success', 'Data Di Update');
     }
-    
+
     public function printQR($request)
     {
         // // $directory = '../public/qrcodes/';
@@ -288,66 +308,66 @@ class EoqtableController extends Controller
     public function print(Request $request)
     {
         $foods = food::all();
-            $cek=false;
-            $lastId = eoqtable::orderBy('id', 'desc')->first()->id; // Mendapatkan ID terakhir
-            $data = [];
+        $cek = false;
+        $lastId = eoqtable::orderBy('id', 'desc')->first()->id; // Mendapatkan ID terakhir
+        $data = [];
+        for ($i = 1; $i <= $lastId; $i++) {
+            $inputName = 'print' . $i;
+            if ($request->$inputName) {
+                $cek = true;
+                break;
+            } elseif (!$request->$inputName) {
+                $cek = false;
+            }
+        }
+        if ($cek) {
             for ($i = 1; $i <= $lastId; $i++) {
-                $inputName = 'print' . $i;
-                if($request->$inputName){
-                    $cek=true;
-                    break;
-                }elseif(!$request->$inputName){
-                    $cek=false;
+                $inputName = 'print' . $i; // Membuat nama input berdasarkan iterasi
+
+                if ($request->has($inputName)) {
+                    // Melakukan pencarian berdasarkan nilai input dari request pada model barangKeluar
+                    $foundMaterial = eoqtable::find($request->$inputName);
+
+                    if ($foundMaterial) {
+                        $data[] = $foundMaterial; // Menambahkan model yang cocok ke dalam array $data
+                    }
                 }
             }
-            if($cek){
-                for ($i = 1; $i <= $lastId; $i++) {
-                    $inputName = 'print' . $i; // Membuat nama input berdasarkan iterasi
-    
-                    if ($request->has($inputName)) {
-                        // Melakukan pencarian berdasarkan nilai input dari request pada model barangKeluar
-                        $foundMaterial = eoqtable::find($request->$inputName);
-    
-                        if ($foundMaterial) {
-                            $data[] = $foundMaterial; // Menambahkan model yang cocok ke dalam array $data
-                        }
-                    }
+            $pdf = Pdf::loadView('stuffoutf.pdf', [
+                "foods" => $foods,
+                "eoqtables" => $data
+            ])->setPaper('f4', 'landscape');
+            return $pdf->download('Laporan_Barang_Keluar.pdf');
+        } else {
+            $eoqtables = eoqtable::query();
+            if ($request->search) {
+                $querytambahans = food::where('peruntukan', 'like', '%' . request('search') . '%')
+                    ->orWhere('namaMaterial', 'like', '%' . request('search') . '%')
+                    ->orWhere('satuan', 'like', '%' . request('search') . '%')->get();
+
+                $eoqtables->where('kodeMaterial', 'like', '%' . request('search') . '%')
+                    ->orWhere('jumlah', 'like', '%' . request('search') . '%')
+                    ->orWhere('kondisi', 'like', '%' . request('search') . '%')
+                    ->orWhere('keterangan', 'like', '%' . request('search') . '%')
+                    ->orWhere('keperluan', 'like', '%' . request('search') . '%')
+                    ->orWhere('keterangan', 'like', '%' . request('search') . '%')
+                    ->orWhere('peminjam', 'like', '%' . request('search') . '%')
+                    ->orWhere('divisi', 'like', '%' . request('search') . '%')
+                    ->orWhere('tanggalKeluar', 'like', '%' . request('search') . '%');
+
+                foreach ($querytambahans as $querytambahan) {
+                    $querybantuan = (string)$querytambahan->kodeMaterial;
+                    $eoqtables->orWhere('kodeMaterial', 'like', '%' . $querybantuan . '%');
                 }
-                $pdf = Pdf::loadView('stuffoutf.pdf', [
-                    "foods" => $foods,
-                    "eoqtables" => $data
-                ])->setPaper('f4', 'landscape');
-                return $pdf->download('Laporan_Barang_Keluar.pdf');
-            }else{
-                $eoqtables = eoqtable::query();
-                if($request->search){
-                    $querytambahans=food::where('peruntukan','like','%'.request('search').'%')
-                                        ->orWhere('namaMaterial','like','%'.request('search').'%')
-                                        ->orWhere('satuan','like','%'.request('search').'%')->get();
-                                        
-                    $eoqtables->where('kodeMaterial','like','%'.request('search').'%')
-                                                ->orWhere('jumlah','like','%'.request('search').'%')
-                                                ->orWhere('kondisi','like','%'.request('search').'%')
-                                                ->orWhere('keterangan','like','%'.request('search').'%')
-                                                ->orWhere('keperluan','like','%'.request('search').'%')
-                                                ->orWhere('keterangan','like','%'.request('search').'%')
-                                                ->orWhere('peminjam','like','%'.request('search').'%')
-                                                ->orWhere('divisi','like','%'.request('search').'%')
-                                                ->orWhere('tanggalKeluar','like','%'.request('search').'%');
-                    
-                    foreach($querytambahans as $querytambahan){
-                        $querybantuan= (string)$querytambahan->kodeMaterial;
-                        $eoqtables->orWhere('kodeMaterial','like','%'.$querybantuan.'%');
-                    }
-                }
-                $foods = $foods->toArray();
-                $eoqtables = $eoqtables->get()->toArray();
-                $pdf = Pdf::loadView('EOQMethod.EOQMethodPrint', [
-                    "foods" => $foods,
-                    "eoqtables" => $eoqtables
-                ])->setPaper('f4', 'landscape');
-                return $pdf->download('EOQMethod.pdf');
-            } 
+            }
+            $foods = $foods->toArray();
+            $eoqtables = $eoqtables->get()->toArray();
+            $pdf = Pdf::loadView('EOQMethod.EOQMethodPrint', [
+                "foods" => $foods,
+                "eoqtables" => $eoqtables
+            ])->setPaper('f4', 'landscape');
+            return $pdf->download('EOQMethod.pdf');
+        }
     }
 
     public function create()
@@ -360,7 +380,6 @@ class EoqtableController extends Controller
      */
     public function store(StoreeoqtableRequest $request)
     {
-       
     }
 
     /**
